54f8a58ca2a6555cf2ea10b13e5f4e3b61dae171
Amer <amermmhamad@gmail.com>
2025-09-16 22:19:42 +0300

    Added 3 buttons for each size

--- alx-project-0x00/interfaces/index.ts
+++ alx-project-0x00/interfaces/index.ts
@@ -1,3 +1,13 @@
 export interface PillProps {
   title: string;
 }
+
+export interface ButtonProps {
+  title: string;
+  size: "sm" | "md" | "lg";
+  shape: "sm" | "md" | "full";
+  variant?: "primary" | "secondary";
+  disabled?: boolean;
+  onClick?: () => void;
+  className?: string;
+}
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,10 +1,14 @@
 import Card from "@/components/Card";
+import Button from "@/components/Button";
 
 const Landing: React.FC = () => {
   return (
     <div>
       <h1 className="text-xl font-extralight">Landing Page</h1>
       <Card />
+      <Button title="Smalle Rounded" size="sm" shape="sm" />
+      <Button title="Smalle Rounded" size="md" shape="md" />
+      <Button title="Smalle Rounded" size="lg" shape="full" />
     </div>
   );
 };
